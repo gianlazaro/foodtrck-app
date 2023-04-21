949d09c68f638d6d120a8820b4bf8cd4560ccd49
Gian Lazaro <hello@gianlazaro.com>
2023-04-20 22:26:37 -0700

    Place an catch block for debugging

--- src/routes/subscription/+server.js
+++ src/routes/subscription/+server.js
@@ -4,11 +4,16 @@ const pb = new PocketBase('https://pb.foodtrck.app');
 
 export function POST({ request }) {
 	return new Response(
-		request.json().then(async (data) => {
-			// send data obj to server
-			await pb.collection('notification_subscriptions').create({
-				keys: data
-			});
-		})
+		request
+			.json()
+			.then(async (data) => {
+				// send data obj to server
+				await pb.collection('notification_subscriptions').create({
+					keys: data
+				});
+			})
+			.catch((err) => {
+				console.error('ABC: ' + err);
+			})
 	);
 }
